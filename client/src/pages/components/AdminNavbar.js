28a0f249d65915ed9a1ca9b9298ab71fa1c8b92c
Aaryan <aaryan24602@gmail.com>
2024-02-25 17:17:18 +0530

    added admin logout

--- client/src/pages/components/AdminNavbar.js
+++ client/src/pages/components/AdminNavbar.js
@@ -17,6 +17,18 @@ function AdminNavbar({ textColor }) {
         >
           Verifier
         </Navbar.Brand>
+
+        <Navbar.Toggle />
+        <Navbar.Collapse className="justify-content-end">
+          <Nav className="ml-auto">
+            <Nav.Link
+              href="/"
+              style={{ marginRight: "25px", color: textColor }}
+            >
+              Logout
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
       </Container>
     </Navbar>
   );
